e66b095242db0dced099468c153ace70b995f676
Yigit <71655773+ByFisher@users.noreply.github.com>
2024-02-27 21:29:01 +0300

    Update toplumesaj.py

--- toplumesaj.py
+++ toplumesaj.py
@@ -1,17 +1,20 @@
-import datatime
+import datetime
 import pywhatkit
 import time 
-with open("phone.txt", "r") as f:
-	lines = f.readlines()
 
-for i in range (len(lines)):
-	lines[i] = lines[i].reaplace(" ", "").strip()
-while datatime.datatime.now().day != 20:
-	time.sleep(60*60)
-while datatime.datatime.now().day != 5:
-	time.sleep(60*5)
-mesaj = "denmeme amaclı mesaj icerıgi"
+with open("phone.txt", "r") as f:
+    lines = f.readlines()
+
+# Telefon numaralarının başındaki ve sonundaki boşlukları ve satır sonu karakterlerini temizle
+lines = [number.strip() for number in lines]
+
+while datetime.datetime.now().day != 0.50:
+    time.sleep(60*60)
+
+while datetime.datetime.now().day != 2:
+    time.sleep(60*5)
+
+mesaj = "deneme amacıyla mesaj iceriği"
 for number in lines:
-	t = datatime.datatime.now()
-	pywhatkit.sendwhatmsg(number, mesaj, t.hour, t.time,+ 1)
- #pywhatkıt.sendwhatmsg_to_group(group_id, message, time_hour, time_min)
+    t = datetime.datetime.now()
+    pywhatkit.sendwhatmsg(number, mesaj, t.hour, t.minute + 1)  # Dakikayı bir arttır
